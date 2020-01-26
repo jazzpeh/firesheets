821d0818ece25c916735eeb6d31bcb69b70e6a12
Jazz Peh <jazzpehz@gmail.com>
2020-01-26 23:38:39 +0800

    Create index.js

---
+++ util/index.js
@@ -0,0 +1,11 @@
+const Database = require('./database');
+const FileReader = require('./file-reader');
+const GSheets = require('./gsheets');
+const StringConverter = require('./string-converter');
+
+module.exports = {
+  Database,
+  FileReader,
+  GSheets,
+  StringConverter,
+};
